750a8aa4ff36742e059c0c02c42f811d5d78fe5e
Edgar Avila <edgaravila01@yahoo.com>
2023-04-15 01:54:08 -0700

    phase 1 arrow functions

---
+++ index.js
@@ -0,0 +1,12 @@
+const divide = function() {
+    const divide = 2000 / 100
+    return divide
+}
+const square = x => x * x
+
+const add = (a , b) => {
+    console.log(`Adding ${a}`);
+    console.log(`Adding ${b}`);
+    return a + b;
+}
+add(a,b);
